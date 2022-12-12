dc6ff6efd6db856a06f806fb474ec78be01ec19f
EosReign <vsevolod_sol@mail.ru>
2022-12-13 02:21:36 +0300

    meh

---
+++ src/main/java/prosky/sphw45rightver/repository/FacultyRepository.java
@@ -0,0 +1,13 @@
+package prosky.sphw45rightver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import prosky.sphw45rightver.entity.Faculty;
+
+import java.util.Collection;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+    Collection<Faculty> findAllByNameIgnoreCase(String name);
+    Collection<Faculty> findAllByColorIgnoreCase(String color);
+}
